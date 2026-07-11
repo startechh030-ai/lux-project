b9bae9935c77088c69ff72a76b9bba8174cf6737
Smart <startechh030@gmail.com>
2026-07-12 00:17:04 +0100

    Fresh project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,15 @@ android {
     }
     buildFeatures { viewBinding = false }
     packaging { jniLibs { useLegacyPackaging = false } }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     kotlinOptions { jvmTarget = "17" }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
